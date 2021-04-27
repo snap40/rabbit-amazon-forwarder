0091846543d3125f0d980f9eb3aff975cbe7ae48
Jim Croft <jim.croft@currenthealth.com>
2021-04-27 17:11:04 +0100

    Set listening port from env var

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/AirHelp/rabbit-amazon-forwarder/mapping"
 	"github.com/AirHelp/rabbit-amazon-forwarder/supervisor"
 	log "github.com/sirupsen/logrus"
@@ -11,6 +12,23 @@ import (
 const (
 	LogLevel = "LOG_LEVEL"
 )
+
+func getListenerAddress() string {
+	var port string
+	addressPattern := "0.0.0.0:%s"
+
+	port = os.Getenv("NOMAD_PORT_http")
+	if port != "" {
+		return fmt.Sprintf(addressPattern, port)
+	}
+
+	port = os.Getenv("SERVER_PORT")
+	if port != "" {
+		return fmt.Sprintf(addressPattern, port)
+	}
+
+	return fmt.Sprintf(addressPattern, "8080")
+}
 
 func main() {
 	createLogger()
@@ -26,7 +44,7 @@ func main() {
 	http.HandleFunc("/restart", supervisor.Restart)
 	http.HandleFunc("/health", supervisor.Check)
 	log.Info("Starting http server")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(getListenerAddress(), nil))
 }
 
 func createLogger() {
